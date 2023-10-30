81f92d945ff6c680636665410be791e6c933c181
tamashorvath1 <146861802+tamashorvath1@users.noreply.github.com>
2023-10-30 21:30:43 +0100

    Add files via upload

--- TrainingProcess/input_data.py
+++ TrainingProcess/input_data.py
@@ -15,6 +15,7 @@
 """Model definitions for simple speech recognition.
 
 """
+import os
 import hashlib
 import math
 import os.path
@@ -210,34 +211,36 @@ class AudioProcessor(object):
       data_url: Web location of the tar file containing the data set.
       dest_directory: File path to extract data to.
     """
-    if not data_url:
+    if not data_url:   
       return
     if not gfile.Exists(dest_directory):
       os.makedirs(dest_directory)
-    filename = data_url.split('/')[-1]
-    filepath = os.path.join(dest_directory, filename)
-    if not gfile.Exists(filepath):
+      filename = data_url.split('/')[-1]
+      filepath = os.path.join(dest_directory, filename)
+      if not gfile.Exists(filepath):
 
-      def _progress(count, block_size, total_size):
-        sys.stdout.write(
-            '\r>> Downloading %s %.1f%%' %
-            (filename, float(count * block_size) / float(total_size) * 100.0))
-        sys.stdout.flush()
+        def _progress(count, block_size, total_size):
+          sys.stdout.write(
+              '\r>> Downloading %s %.1f%%' %
+              (filename, float(count * block_size) / float(total_size) * 100.0))
+          sys.stdout.flush()
 
-      try:
-        filepath, _ = urllib.request.urlretrieve(data_url, filepath, _progress)
-      except:
-        tf.compat.v1.logging.error(
+        try:
+          filepath, _ = urllib.request.urlretrieve(data_url, filepath, _progress)
+        except:
+          tf.compat.v1.logging.error(
             'Failed to download URL: {0} to folder: {1}. Please make sure you '
             'have enough free space and an internet connection'.format(
                 data_url, filepath))
-        raise
-      print()
-      statinfo = os.stat(filepath)
-      tf.compat.v1.logging.info(
+          raise
+        print()
+        statinfo = os.stat(filepath)
+        tf.compat.v1.logging.info(
           'Successfully downloaded {0} ({1} bytes)'.format(
               filename, statinfo.st_size))
-      tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+    else:
+      return
 
   def prepare_data_index(self, silence_percentage, unknown_percentage,
                          wanted_words, validation_percentage,
