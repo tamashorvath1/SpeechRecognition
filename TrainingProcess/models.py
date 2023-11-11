38a135f801f26a561a71ec715c97ccfc48c5d69a
tamashorvath1 <146861802+tamashorvath1@users.noreply.github.com>
2023-11-12 00:57:54 +0100

    Update models.py

--- TrainingProcess/models.py
+++ TrainingProcess/models.py
@@ -900,7 +900,153 @@ def create_tiny_embedding_conv_model(fingerprint_input, model_settings,
   else:
     return final_fc
 
+
 def myModelThesis(fingerprint_input, model_settings,
+                                     is_training):
+  """Builds a convolutional model for my thesis.
+
+  Here's the layout of the graph:
+  conv_model + 2 conv layer + 1 fc layer
+  (fingerprint_input)
+          v
+      [Conv2D]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+        [Relu]
+          v
+      [MaxPool]
+          v
+      [Conv2D]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+        [Relu]
+          v
+      [MaxPool]
+          v
+      [MatMul]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+
+  Args:
+    fingerprint_input: TensorFlow node that will output audio feature vectors.
+    model_settings: Dictionary of information about the model.
+    is_training: Whether the model is going to be used for training.
+
+  Returns:
+    TensorFlow node outputting logits results, and optionally a dropout
+    placeholder.
+  """
+  if is_training:
+    dropout_prob = tf.compat.v1.placeholder(tf.float32, name='dropout_prob')
+  input_frequency_size = model_settings['fingerprint_width']
+  input_time_size = model_settings['spectrogram_length']
+  fingerprint_4d = tf.reshape(fingerprint_input,
+                              [-1, input_time_size, input_frequency_size, 1])
+  first_filter_width = 8
+  first_filter_height = 20
+  first_filter_count = 64
+  first_weights = tf.compat.v1.get_variable(
+      name='first_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[first_filter_height, first_filter_width, 1, first_filter_count])
+  first_bias = tf.compat.v1.get_variable(
+      name='first_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[first_filter_count])
+
+  first_conv = tf.nn.conv2d(input=fingerprint_4d,
+                            filters=first_weights,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME') + first_bias
+  first_relu = tf.nn.relu(first_conv)
+  if is_training:
+    first_dropout = tf.nn.dropout(first_relu, 1 - (dropout_prob))
+  else:
+    first_dropout = first_relu
+  first_max_pool = tf.nn.max_pool2d(input=first_dropout,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+  second_filter_width = 4
+  second_filter_height = 10
+  second_filter_count = 64
+  second_weights = tf.compat.v1.get_variable(
+      name='second_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[
+          second_filter_height, second_filter_width, second_filter_count,
+          second_filter_count
+      ])
+  second_bias = tf.compat.v1.get_variable(
+      name='second_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[second_filter_count])
+  second_conv = tf.nn.conv2d(input=first_max_pool,
+                             filters=second_weights,
+                             strides=[1, 1, 1, 1],
+                             padding='SAME') + second_bias
+  second_relu = tf.nn.relu(second_conv)
+  if is_training:
+    second_dropout = tf.compat.v1.nn.dropout(second_relu, dropout_prob)
+  else:
+    second_dropout = second_relu
+  
+  second_conv_shape = second_dropout.get_shape()
+  second_conv_output_width = second_conv_shape[2]
+  second_conv_output_height = second_conv_shape[1]
+  second_conv_element_count = int(
+      second_conv_output_width * second_conv_output_height *
+      second_filter_count)
+  flattened_second_conv = tf.reshape(second_dropout,
+                                     [-1, second_conv_element_count])
+  
+  first_fc_output_channels = 128
+  first_fc_weights = tf.compat.v1.get_variable(
+      name='first_fc_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[third_conv_element_count, first_fc_output_channels])
+  first_fc_bias = tf.compat.v1.get_variable(
+      name='first_fc_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[first_fc_output_channels])
+  first_fc = tf.matmul(flattened_second_conv, first_fc_weights) + first_fc_bias
+  if is_training:
+    second_fc_input = tf.nn.dropout(first_fc, 1 - (dropout_prob))
+  else:
+    second_fc_input = first_fc
+  second_fc_output_channels = 128
+  second_fc_weights = tf.compat.v1.get_variable(
+      name='second_fc_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[first_fc_output_channels, second_fc_output_channels])
+  second_fc_bias = tf.compat.v1.get_variable(
+      name='second_fc_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[second_fc_output_channels])
+  second_fc = tf.matmul(second_fc_input, second_fc_weights) + second_fc_bias
+  if is_training:
+    final_fc_input = tf.nn.dropout(second_fc, 1 - (dropout_prob))
+  else:
+    final_fc_input = second_fc
+  label_count = model_settings['label_count']
+  final_fc_weights = tf.compat.v1.get_variable(
+      name='final_fc_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[second_fc_output_channels, label_count])
+  final_fc_bias = tf.compat.v1.get_variable(
+      name='final_fc_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[label_count])
+  final_fc = tf.matmul(final_fc_input, final_fc_weights) + final_fc_bias
+  if is_training:
+    return final_fc, dropout_prob
+  else:
+    return final_fc
+
+def myModelThesisv1(fingerprint_input, model_settings,
                                      is_training):
   """Builds a convolutional model for my thesis.
 
